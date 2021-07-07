36fb7587cd19e62bf3f410b96fe5aa209ac60d6a
brianlball <brian.ball@nrel.gov>
2021-07-07 14:02:37 -0600

    update classes for willow schema

--- app.py
+++ app.py
@@ -14,65 +14,110 @@ app.config['MONGODB_SETTINGS'] = {
 db = MongoEngine()
 db.init_app(app)
 
-class User(db.Document):
-    _id = db.StringField()
-    dis = db.StringField()
-    siteRef = db.StringField()
-    equipRef = db.StringField()
-    equip = db.StringField()
-    heatPump = db.StringField()
-    fan = db.StringField()
-    point = db.StringField()
-    sensor = db.StringField()
-    sensorFault = db.BooleanField(default=False)
-    speedCmd = db.StringField()
-    temp = db.StringField()
-    value = db.FloatField()
-    
-    def to_json(self):
-        return {"_id": self._id,
-                "dis": self.dis,
-                "siteRef": self.siteRef,
-                "equipRef": self.equipRef,
-                "equip": self.equip,
-                "heatPump": self.heatPump,
-                "fan": self.fan,
-                "point": self.point,
-                "sensor": self.sensor,
-                "sensorFault": self.sensorFault,
-                "speedCmd": self.speedCmd,
-                "temp": self.temp,
-                "value": self.value
-                }
+class Tags(db.EmbeddedDocument):
+    name = db.StringField()
+    feature = db.StringField()
 
-user = User(_id = "ee349ca0-f8aa-4b83-9fc6-86d727399914",
-            dis = "Unitary HP",
-            equip = "m",
-            heatPump = "m",
-            siteRef = "6b25c3c7-39e4-4be7-84a9-17e80feecaf5")
-user.save()            
-user1 = User(_id = "ce731aa1-fb9c-40b9-8f6b-ec6c42c9eadb",
-            dis = "Fan",
-            equip = "m",
-            fan = "m",
-            equipRef = "ee349ca0-f8aa-4b83-9fc6-86d727399914")
-user1.save()    
-user2 = User(_id = "bf34cfc8-cfe2-44d0-bf0a-ce462e2dc1c2",
-             dis = "Return_Air_Temperature_Sensor",
-             temp = "m",
-             sensor = "m",
-             sensorFault = False,
-             value = 72,
-             point = "m",
-             equipRef = "ee349ca0-f8aa-4b83-9fc6-86d727399914")
-user2.save()
-user3 = User(_id = "af34cfc8-cfe2-44d0-bf0a-ce462e2dc1c1",
-            dis = "FanSpeed",
-            speedCmd = "m",
-            value = 0,
-            point = "m",
-            equipRef = "ce731aa1-fb9c-40b9-8f6b-ec6c42c9eadb")
-user3.save()
+class CurrentValue(db.EmbeddedDocument):
+    value = db.StringField()
+    unit = db.StringField()
+    
+class AssetParameters(db.EmbeddedDocument):
+    key = db.StringField()
+    displayName = db.StringField()
+    value = db.StringField()
+
+class Assets(db.EmbeddedDocument):
+    _id = db.StringField()
+    id = db.StringField(db_field='id')
+    name = db.StringField()
+    categoryName = db.StringField()
+
+class Properties(db.EmbeddedDocument):
+    kind = db.StringField()
+    displayName = db.StringField()
+    value = db.StringField()
+
+class Metadata(db.EmbeddedDocument):
+    key = db.StringField()
+    dataType = db.StringField()
+    value = db.StringField()
+
+class Points(db.EmbeddedDocument):
+    _id = db.StringField()
+    id = db.StringField(db_field='id')
+    modelId = db.StringField()
+    twinId = db.StringField()
+    trendId = db.StringField()
+    name = db.StringField()
+    description = db.StringField()
+    tags = db.EmbeddedDocumentListField(Tags)
+    type = db.StringField()
+    currentValue = db.EmbeddedDocumentListField(CurrentValue)
+    displayPriority = db.IntField()
+    assets = db.EmbeddedDocumentListField(Assets)
+    type = db.StringField()
+    trenedInterval = db.IntField()
+    isEnabled = db.BooleanField(default=False)
+    isDetected = db.BooleanField(default=False)
+    deviceId = db.StringField()
+    categoryName = db.StringField()
+    properties = db.EmbeddedDocumentListField(Properties)
+    metadata = db.EmbeddedDocumentListField(Metadata)
+    
+class Asset(db.DynamicDocument):
+    _id = db.StringField()
+    id = db.StringField(db_field='id')
+    modelId = db.StringField()
+    twinId = db.StringField()
+    name = db.StringField()
+    hasLiveData = db.BooleanField(default=False)
+    categoryId = db.StringField()
+    categoryName = db.StringField()
+    floorId = db.StringField()
+    identifier = db.StringField()
+    forgeViewerModelId = db.StringField()
+    tags = db.EmbeddedDocumentListField(Tags)
+    points = db.EmbeddedDocumentListField(Points)
+    parentId = db.StringField()
+    assetParameters = db.EmbeddedDocumentListField(AssetParameters)
+
+asset = Asset(_id = "ee349ca0-f8aa-4b83-9fc6-86d727399914",
+            modelId = "Unitary HP",
+            twinId = "6b25c3c7-39e4-4be7-84a9-17e80feecaf5")
+asset.save()            
+asset1 = Asset(_id = "ce731aa1-fb9c-40b9-8f6b-ec6c42c9eadb",
+            modelId = "Fan",
+            name = "ee349ca0-f8aa-4b83-9fc6-86d727399914")
+asset1.save()    
+asset2 = Asset(_id = "bf34cfc8-cfe2-44d0-bf0a-ce462e2dc1c2",
+             modelId = "Return_Air_Temperature_Sensor",
+             name = "ee349ca0-f8aa-4b83-9fc6-86d727399914")
+asset2.save()
+asset3 = Asset(_id = "af34cfc8-cfe2-44d0-bf0a-ce462e2dc1c1",
+            modelId = "FanSpeed",
+            name = "ce731aa1-fb9c-40b9-8f6b-ec6c42c9eadb")
+asset3.save()
+
+asset4 = Asset(_id = "62b1928c-7331-02a3-bc74-3dba18ca91a2",
+                modelId = "dtmi:com:willowinc:FanPoweredBox;1",
+                twinId = "WLW-NYC-575_5_AVE-FPB-17.02",
+                name = "Variable Air Volume Box VAV-01.1",
+                hasLiveData = True,
+                categoryId = "00300000-0000-0000-0000-000000010976",
+                categoryName = "Fan Powered Box",
+                floorId = "b7d170fd-48fd-4392-a4e9-1ad5880edc62",
+                identifier = "VAV-01.1",
+                forgeViewerModelId = "b7d170fd-48fd-4392-a4e9-1ad5880edc62")
+#asset4.tags = [
+#    Tags(name="equip"),
+#    Tags(name="havc")
+#]
+tag = Tags(name="equip")
+asset4.tags.append(tag)
+tag = Tags(name="hvac")
+asset4.tags.append(tag)
+asset4.save()
 
 #
 #r = requests.get('http://localhost:5002/', params = {"_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914"})
@@ -81,27 +126,27 @@ user3.save()
 @app.route('/', methods=['GET'])
 def query_records():
     _id = request.args.get('_id')
-    user = User.objects(_id=_id).first()
-    if not user:
+    asset = Asset.objects(_id=_id).first()
+    if not asset:
         return jsonify({'error': 'data not found'})
     else:
-        return jsonify(user.to_json())
+        return jsonify(asset.to_json())
 
 #
 #http://localhost:5002/objects/ee349ca0-f8aa-4b83-9fc6-86d727399914
 #
 @app.route('/objects/<id>')
 def get_one_object(id: str):
-    user = User.objects.get_or_404(_id=id)
-    return user.to_json(), 200
+    asset = Asset.objects.get_or_404(_id=id)
+    return asset.to_json(), 200
 
 #
 #http://localhost:5002/all_objects
 #
 @app.route('/all_objects')
 def get_all_objects():
-    user = User.objects()
-    return user.to_json(), 200
+    asset = Asset.objects()
+    return asset.to_json(), 200
     
 #
 #r = requests.put('http://localhost:5002/update', json = {"_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "fan": "n"})
@@ -111,14 +156,14 @@ def get_all_objects():
 def update_object():
     body = request.get_json()
     _id = body['_id']
-    user = User.objects(_id=_id)
-    if not user:
+    asset = Asset.objects(_id=_id)
+    if not asset:
         return jsonify({'error': 'data were not found'})
     else:
-        user.update(**body)
-        return jsonify(user.to_json())
+        asset.update(**body)
+        return jsonify(asset.to_json())
 #
-##HP_json = '{ "_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "dis": "Unitary HP", "fan": "m", "heatPump": "m", "siteRef": "6b25c3c7-39e4-4be7-84a9-17e80feecaf5"}'
+##HP_json = '{ "_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "modelId": "Unitary HP", "fan": "m", "heatPump": "m", "twinId": "6b25c3c7-39e4-4be7-84a9-17e80feecaf5"}'
 #r = requests.put('http://localhost:5002/', data = HP_json)
 # THIS ADDS 'fan: m' THRU UPDATE
 #
@@ -126,34 +171,34 @@ def update_object():
 def create_record():
     record = json.loads(request.data)
     _id = record['_id']
-    user = User.objects(_id=_id)
-    if not user:
+    asset = Asset.objects(_id=_id)
+    if not asset:
         return jsonify({'error': 'data not found'})
     else:
-        user.update(**record)
-        return jsonify(user.to_json())
+        asset.update(**record)
+        return jsonify(asset.to_json())
 #
-#HP_json = '{ "_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "dis": "Unitary HP", "equip": "m", "heatPump": "m", "siteRef": "6b25c3c7-39e4-4be7-84a9-17e80feecaf5"}'
+#HP_json = '{ "_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914", "modelId": "Unitary HP", "equip": "m", "heatPump": "m", "twinId": "6b25c3c7-39e4-4be7-84a9-17e80feecaf5"}'
 #r = requests.post('http://localhost:5002/', data = HP_json)
 #
 @app.route('/', methods=['POST'])
 def update_record():
     record = json.loads(request.data)
     print(record)
-    user = User(**record).save()
-    return jsonify(user.to_json())
+    asset = Asset(**record).save()
+    return jsonify(asset.to_json())
 #
 #r = requests.delete('http://localhost:5002/', params = {"_id": "ee349ca0-f8aa-4b83-9fc6-86d727399914"})
 #
 @app.route('/', methods=['DELETE'])
 def delete_record():
     _id = request.args.get('_id')
-    user = User.objects(_id=_id)
-    if not user:
+    asset = Asset.objects(_id=_id)
+    if not asset:
         return jsonify({'error': 'data not found'})
     else:
-        user.delete()
-    return jsonify(user.to_json())
+        asset.delete()
+    return jsonify(asset.to_json())
 
 if __name__ == '__main__':
     # run app in debug mode on port 5001
